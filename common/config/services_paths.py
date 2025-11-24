15e078a779c7b6c9acb570cb22c42248fdeac35a
Олександр Беркещук <sanya.bero@gmail.com>
2025-11-24 19:31:20 +0200

    first commit

--- common/config/services_paths.py
+++ common/config/services_paths.py
@@ -1,2 +1,2 @@
-USER_SERVICE_URL = "https://python-web-3.onrender.com/"
-HOTEL_SERVICE_URL = "https://python-web-3.onrender.com/"
+USER_SERVICE_URL = "https://python-web-3.onrender.com"
+HOTEL_SERVICE_URL = "https://python-web-3.onrender.com"
