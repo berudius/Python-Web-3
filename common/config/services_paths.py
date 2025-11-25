044ea66cb6dc69dcd3a1aa2b46260e60f4a5e94d
Berkeshchuk Oleksandr <167199823+berudius@users.noreply.github.com>
2025-11-25 16:56:43 +0200

    Update services_paths.py

--- common/config/services_paths.py
+++ common/config/services_paths.py
@@ -1,4 +1,4 @@
-USER_SERVICE_URL = "https://edemium.onrender.com"
-HOTEL_SERVICE_URL = "https://edemium.onrender.com"
+USER_SERVICE_URL = "https://edemium-iikb.onrender.com"
+HOTEL_SERVICE_URL = "https://edemium-iikb.onrender.com"
 # USER_SERVICE_URL = "http://localhost:5000"
 # HOTEL_SERVICE_URL = "http://localhost:5000"
