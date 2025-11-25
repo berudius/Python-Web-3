d6f56eefd21f168e443c829b72be34cb588c3fa7
Олександр Беркещук <sanya.bero@gmail.com>
2025-11-25 11:20:42 +0200

    user_service completed

--- common/config/services_paths.py
+++ common/config/services_paths.py
@@ -1,2 +1,4 @@
-USER_SERVICE_URL = "https://python-web-3.onrender.com"
-HOTEL_SERVICE_URL = "https://python-web-3.onrender.com"
+# USER_SERVICE_URL = "https://python-web-3.onrender.com"
+# HOTEL_SERVICE_URL = "https://python-web-3.onrender.com"
+USER_SERVICE_URL = "http://localhost:5000"
+HOTEL_SERVICE_URL = "http://localhost:5000"
