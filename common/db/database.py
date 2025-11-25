6a140d5516986b14fa90d4469cb975d395acfa75
Berkeshchuk Oleksandr <167199823+berudius@users.noreply.github.com>
2025-11-25 15:58:12 +0200

    Update database.py

--- common/db/database.py
+++ common/db/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-engine = create_engine("postgresql://neondb_owner:npg_TdVPw4JbyB8A@ep-orange-pine-a4d5ngsg-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require", echo=True)
+engine = create_engine("postgresql://neondb_owner:npg_Xact3pNrR2HO@ep-delicate-glitter-agm9c39m-pooler.c-2.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require", echo=True)
 # engine = create_engine("mysql+mysqlconnector://root:admin@localhost:3306/python_db", echo=True)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
