47bf6d9123235751ceca4d411d15ff5c3879bc40
Олександр Беркещук <sanya.bero@gmail.com>
2025-11-25 11:28:13 +0200

    first commit

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 from user_service.app.backend.routers import auth_router
 from common.db.database import Base, engine
-from hotel_service.app.backend.routers import  public_router, services_router, about_us_router, rooms_router, booking_router, admin_panel_router
-from hotel_service.app.backend.config.statica_config import static_dir_path
-# from common.docker.redis_launcher import run_redis, stop_redis
 from contextlib import asynccontextmanager
 
 @asynccontextmanager
@@ -20,14 +16,6 @@ async def lifespan(app: FastAPI):
     #     stop_redis()
 
 app = FastAPI(lifespan=lifespan)
-app.mount("/static", StaticFiles(directory=static_dir_path), name="static")
 
 app.include_router(auth_router.router)
-
-app.include_router(public_router.router)
-app.include_router(services_router.router)
-app.include_router(about_us_router.router)
-app.include_router(rooms_router.router)
-app.include_router(booking_router.router)
-app.include_router(admin_panel_router.router)
 
