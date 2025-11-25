fb7f4a16530b1da3850edd651a1e4e13fc99612b
Berkeshchuk Oleksandr <167199823+berudius@users.noreply.github.com>
2025-11-25 15:38:50 +0200

    Update user.py

--- common/pydantic/user.py
+++ common/pydantic/user.py
@@ -6,7 +6,6 @@ class UserUpdatePayload(BaseModel):
     phone_number: Optional[str] = None
     role: Optional[str] = None
     
-    # Додаємо поля для ігрової механіки
     trust_level: Optional[int] = None
     consecutive_cancellations: Optional[int] = None
 
